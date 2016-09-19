231f6a360bacbcff5eff5dbc2e83961aea2c2d17
Andy Anderson <andyanderson1818@gmail.com>
2016-09-19 15:35:18 -0700

    Bcrypt. Added a migration file and edited said file. Schema updated via process

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919181921) do
+ActiveRecord::Schema.define(version: 20160919220457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,9 @@ ActiveRecord::Schema.define(version: 20160919181921) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
